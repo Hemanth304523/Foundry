09afba4f5c3459d89d3a4c784c04d2f39087e10f
Hemanth Bugata <102855092+Hemanthbugata@users.noreply.github.com>
2026-01-16 17:17:09 +0530

    complete testing

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,4 +1,6 @@
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
